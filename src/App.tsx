6a5c7de9e38df7a2a6c2c9bcc23733ece2b7bd16
DannyHT04 <dannyhat1@gmail.com>
2024-02-07 11:54:12 -0800

    Finished DrumKit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Soundbutton from './components/soundbutton';
+import clap from "./assets/clap.wav"
+import hihat from "./assets/hihat.wav"
+import kick from "./assets/kick.wav"
+import openhat from "./assets/openhat.wav"
+import boom from "./assets/boom.wav"
+import ride from "./assets/ride.wav"
+import snare from "./assets/snare.wav"
+import tom from "./assets/tom.wav"
+import tink from "./assets/tink.wav"
+
+
+export type SoundType = {
+  letter: string;
+  name: string;
+  sound: string;
+}
 
 function App() {
+  const [musicNotes, setMusicNotes] = useState<SoundType[]>([
+    {
+      letter: "A",
+      name: "clap",
+      sound: clap,
+    },
+    {
+      letter: "S",
+      name: "HiHat",
+      sound: hihat,
+    },
+    {
+      letter: "D",
+      name: "Kick",
+      sound: kick,
+    },
+    {
+      letter: "F",
+      name: "OpenHat",
+      sound: openhat,
+    },
+    {
+      letter: "G",
+      name: "Boom",
+      sound: boom,
+    },
+    {
+      letter: "H",
+      name: "Ride",
+      sound: ride,
+    },
+    {
+      letter: "J",
+      name: "Snare",
+      sound: snare,
+    },
+    {
+      letter: "K",
+      name: "tom",
+      sound: tom,
+    },
+    {
+      letter: "L",
+      name: "Tink",
+      sound: tink,
+    },
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='keys'>
+      {
+        musicNotes.map((music, index) => 
+          <Soundbutton key={index} data={music}/>
+          )
+      }
     </div>
   );
 }
---
+++ src/components/soundbutton.tsx
@@ -0,0 +1,24 @@
+import { SoundType } from "../App";
+import useSound from "use-sound";
+import test from "../assets/boom.wav"
+
+type SoundButtonType = {
+ data: SoundType
+}
+const SoundButton = ({data}: SoundButtonType) => {
+  const [play] = useSound(data.sound)
+
+    return(
+        <div className='key' onClick={() =>{
+          console.log("first")
+          // console.log(play)
+          play()
+        }}>
+        <p>{data.letter}</p>
+        <span className='sound'>{data.name}</span>
+      </div>
+    )
+    
+  }
+  
+export default SoundButton;
